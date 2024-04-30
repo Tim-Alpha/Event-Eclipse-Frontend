9b9d60e175d293a4c8472298bb1c14a4056be8fd
Tim-Alpha <codewithaisha@gmail.com>
2024-04-30 15:02:59 +0530

    update: ad images to top container

--- src/components/TopContainer.js
+++ src/components/TopContainer.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Header from "./Header";
+import img5 from "./images/img5.jpeg"
+import img6 from "./images/img6.jpeg"
+import img7 from "./images/img7.webp"
+import img8 from "./images/img8.webp"
 
 const TopContainer = () => {
     return (
         <>
             <div className="home_top_container">
+                <div className="slight_up" id="slight_up">
+                    <img className="container_image" alt="test" src={img7} />
+                    <img className="container_image" alt="test" src={img6} />
+                    <img className="container_image" alt="test" src={img8} />
+                    <img className="container_image" alt="test" src={img5} />
+                </div>
                 <Header />
             </div>
         </>
