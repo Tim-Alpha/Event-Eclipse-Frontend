9ee33c20aea0adab966df53a71460df924fbfa9e
Tim-Alpha <codewithaisha@gmail.com>
2024-05-16 00:48:42 +0530

    update: refactor header position

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,11 +10,13 @@ import img5 from "./images/img5.jpeg"
 import img6 from "./images/img6.jpeg"
 import img7 from "./images/img7.webp"
 import img8 from "./images/img8.webp"
+import Header from "./Header";
 
 const Home = () => {
 
     return (
         <>
+            <Header />
             <TopContainer />
             <div className="home_bottom_container">
                 {/* <div className="slight_up" id="slight_up">
@@ -25,18 +27,18 @@ const Home = () => {
                     <Container newClass="small_box" width="30" height="30" background="green" corner="20" />
                 </div> */}
                 <div className="venue_container">
-                    <Container newClass="container" width="30" height="40" background="pink" corner="8" link={img1}/>
-                    <Container newClass="container" width="30" height="40" background="pink" corner="8" link={img2}/>
-                    <Container newClass="container" width="30" height="40" background="pink" corner="8" link={img3}/>
-                    <Container newClass="container" width="30" height="40" background="pink" corner="8" link={img4}/>
-                    <Container newClass="container" width="30" height="40" background="pink" corner="8" link={img5}/>
-                    <Container newClass="container" width="30" height="40" background="pink" corner="8" link={img6}/>
-                    <Container newClass="container" width="30" height="40" background="pink" corner="8" link={img7}/>
-                    <Container newClass="container" width="30" height="40" background="pink" corner="8" link={img8}/>
-                    <Container newClass="container" width="30" height="40" background="pink" corner="8" link={img1}/>
-                    <Container newClass="container" width="30" height="40" background="pink" corner="8" link={img2}/>
-                    <Container newClass="container" width="30" height="40" background="pink" corner="8" link={img3}/>
-                    <Container newClass="container" width="30" height="40" background="pink" corner="8" link={img4}/>
+                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img1}/>
+                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img2}/>
+                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img3}/>
+                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img4}/>
+                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img5}/>
+                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img6}/>
+                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img7}/>
+                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img8}/>
+                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img1}/>
+                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img2}/>
+                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img3}/>
+                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img4}/>
                 </div>
             </div>
             <div className="footer">
