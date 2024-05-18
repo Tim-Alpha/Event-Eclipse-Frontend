10c140bedbc2150637d8a826c18232a04ed967ee
Tim-Alpha <codewithaisha@gmail.com>
2024-05-18 12:59:56 +0530

    update: add UI/UX change for top & bottom bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,60 @@
-import React from "react";
-import Container from "./Container";
-import Footer from "./Footer";
-import TopContainer from "./TopContainer";
-import img1 from "./images/img1.jpg"
-import img2 from "./images/img2.jpg"
-import img3 from "./images/img3.jpeg"
-import img4 from "./images/img4.jpeg"
-import img5 from "./images/img5.jpeg"
-import img6 from "./images/img6.jpeg"
-import img7 from "./images/img7.webp"
-import img8 from "./images/img8.webp"
-import Header from "./Header";
+import React, { useEffect, useState } from 'react';
+import Container from './Container';
+import Footer from './Footer';
+import TopContainer from './TopContainer';
+import img1 from './images/img1.jpg';
+import img2 from './images/img2.jpg';
+import img3 from './images/img3.jpeg';
+import img4 from './images/img4.jpeg';
+import img5 from './images/img5.jpeg';
+import img6 from './images/img6.jpeg';
+import img7 from './images/img7.webp';
+import img8 from './images/img8.webp';
+import Header from './Header';
+import SignupPopup from './SignupPopup';
+import { isLoggedIn } from '../helper/auth';
 
 const Home = () => {
+  const [showPopup, setShowPopup] = useState(false);
 
-    return (
-        <>
-            <Header />
-            <TopContainer />
-            <div className="home_bottom_container">
-                {/* <div className="slight_up" id="slight_up">
-                    <Container newClass="small_box" width="30" height="30" background="green" corner="20" />
-                    <Container newClass="small_box" width="30" height="30" background="green" corner="20" />
-                    <Container newClass="small_box" width="30" height="30" background="green" corner="20" />
-                    <Container newClass="small_box" width="30" height="30" background="green" corner="20" />
-                    <Container newClass="small_box" width="30" height="30" background="green" corner="20" />
-                </div> */}
-                <div className="venue_container">
-                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img1}/>
-                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img2}/>
-                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img3}/>
-                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img4}/>
-                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img5}/>
-                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img6}/>
-                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img7}/>
-                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img8}/>
-                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img1}/>
-                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img2}/>
-                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img3}/>
-                    <Container newClass="container" width="30" height="40" background="" corner="8" link={img4}/>
-                </div>
-            </div>
-            <div className="footer">
-                <Footer />
-            </div>
-        </>
-    );
+  useEffect(() => {
+    if (!isLoggedIn()) {
+      const timer = setTimeout(() => {
+        setShowPopup(true);
+      }, 10000);
+
+      return () => clearTimeout(timer);
+    }
+  }, []);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
+  return (
+    <>
+      <Header />
+      <TopContainer />
+      <div className="container mt-4">
+        <div className="row">
+          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img1} />
+          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img2} />
+          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img3} />
+          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img4} />
+          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img5} />
+          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img6} />
+          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img7} />
+          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img8} />
+          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img1} />
+          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img2} />
+          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img3} />
+          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img4} />
+        </div>
+      </div>
+      <Footer />
+      {showPopup && <SignupPopup onClose={handleClosePopup} />}
+    </>
+  );
 };
 
 export default Home;
--- src/components/TopContainer.js
+++ src/components/TopContainer.js
@@ -1,22 +1,53 @@
-import React from "react";
-import img5 from "./images/img5.jpeg"
-import img6 from "./images/img6.jpeg"
-import img7 from "./images/img7.webp"
-import img8 from "./images/img8.webp"
+import React, { useState, useEffect } from "react";
+import img5 from "./images/img5.jpeg";
+import img6 from "./images/img6.jpeg";
+import img7 from "./images/img7.webp";
+import img8 from "./images/img8.webp";
 
 const TopContainer = () => {
+    const images = [img7, img6, img8, img5];
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) =>
+                prevIndex === images.length - 1 ? 0 : prevIndex + 1
+            );
+        }, 2000);
+
+        return () => clearInterval(interval);
+    }, [images.length]);
+
+    const handleDotClick = (index) => {
+        setCurrentImageIndex(index);
+    };
+
     return (
         <>
             <div className="home_top_container">
                 <div className="slight_up" id="slight_up">
-                    <img className="container_image" alt="test" src={img7} />
-                    <img className="container_image" alt="test" src={img6} />
-                    <img className="container_image" alt="test" src={img8} />
-                    <img className="container_image" alt="test" src={img5} />
+                    {images.map((img, index) => (
+                        <img
+                            key={index}
+                            className="container_image"
+                            alt={`test ${index}`}
+                            src={img}
+                            style={{ display: index === currentImageIndex ? "block" : "none" }}
+                        />
+                    ))}
+                    <div className="dots-container">
+                        {images.map((_, index) => (
+                            <div
+                                key={index}
+                                className={`dot ${index === currentImageIndex ? "active" : ""}`}
+                                onClick={() => handleDotClick(index)}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default TopContainer;
