77bb434b7979911783e57b4f8d9e3f835fce6e00
Sachin Kinha <codewithaisha@gmail.com>
2024-07-07 00:26:59 +0530

    update: implement login page

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,87 +1,92 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
-import axios from 'axios';
-
-const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('https://eventeclipsebackend.onrender.com/api/v1/users/login', { 
-        username: username, 
-        password: password 
-      });
-
-      const data = response.data;
-
-      if (data.status === 'success') {
-        localStorage.setItem('token', data.token);
-        localStorage.setItem('user', JSON.stringify(data.user));
-        alert('Login successful');
-        navigate('/home');
-      } else {
-        alert('Login failed: ' + data.message);
-      }
-    } catch (error) {
-      console.error("Login Failed:", error);
-      alert("Login Failed. Please try again.");
-    }
-  };
-
-  return (
-    <>
-      <Header />
-      <div className="container">
-        <div className="row justify-content-center mt-5">
-          <div className="col-md-6 form-container-mb-100">
-            <div className="card">
-              <div className="card-body">
-                <h3 className="card-title text-center">Login</h3>
-                <form onSubmit={handleLogin}>
-                  <div className="form-group">
-                    <label htmlFor="username">Username</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="username"
-                      value={username}
-                      onChange={(e) => setUsername(e.target.value)}
-                      required
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      required
-                    />
-                  </div>
-                  <button type="submit" className="btn btn-primary btn-block mt-2">
-                    Login
-                  </button>
-                </form>
-                <div className="text-center mt-3">
-                  <button className="btn btn-link" onClick={() => navigate('/register')}>
-                    Don't have an account? Register here.
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
-};
-
+import React, { useState } from 'react'; 
+import { useNavigate } from 'react-router-dom'; 
+import Header from '../header-footer/Header';
+// import Footer from '../header-footer/Footer'; 
+import "../css/Login.css"; 
+import log from "../assets/Log.json";
+import Lottie from 'lottie-react'; 
+import { FaUser, FaLock } from "react-icons/fa"; 
+import axios from 'axios'; 
+ 
+const Login = () => { 
+  const [username, setUsername] = useState(''); 
+  const [password, setPassword] = useState(''); 
+  const navigate = useNavigate(); 
+ 
+  const handleLogin = async (e) => { 
+    e.preventDefault(); 
+    try { 
+      const response = await axios.post('https://eventeclipsebackend.onrender.com/api/v1/users/login', {  
+        username: username,  
+        password: password  
+      }); 
+ 
+      const data = response.data; 
+ 
+      if (data.status === 'success') { 
+        localStorage.setItem('token', data.token); 
+        localStorage.setItem('user', JSON.stringify(data.user)); 
+        alert('Login successful'); 
+        navigate('/home'); 
+      } else { 
+        alert('Login failed: ' + data.message); 
+      } 
+    } catch (error) { 
+      console.error("Login Failed:", error); 
+      alert("Login Failed. Please try again."); 
+    } 
+  }; 
+ 
+  return ( 
+    <> 
+      <Header /> 
+      <div className='main'> 
+        <div style={{ width: "25%"}}> 
+          <Lottie animationData={log}/> 
+        </div> 
+      <div className="container"> 
+      <form onSubmit={handleLogin}> 
+        <h1>Login</h1> 
+          <div className="input-box"> 
+            <input 
+            type="text" 
+              placeholder="Username" 
+              id="username" 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} 
+              required 
+            /> 
+            <FaUser className='icon'/> 
+          </div> 
+          <div className="input-box"> 
+            <input 
+              type="password" 
+              placeholder='Password' 
+              id="password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              required 
+            /> 
+            <FaLock className='icon'/> 
+          </div> 
+ 
+          <div className="remember"> 
+            <label><input type="checkbox"/>Remember me</label> 
+            <a href="#">Forgot Password?</a> 
+          </div> 
+ 
+          <button type="submit"> Login </button> 
+        <div className="register-link"> 
+          <p onClick={() => navigate('/register')}> 
+            Don't have an account? Register here. 
+          </p> 
+        </div> 
+        </form> 
+      </div> 
+      {/* <Footer /> */} 
+      </div> 
+    </> 
+  ); 
+}; 
+ 
 export default Login;
