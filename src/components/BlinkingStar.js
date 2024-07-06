2e6f4acc6f63fab99b516b8bd1502d099a7230d3
Sachin Kinha <codewithaisha@gmail.com>
2024-07-06 18:22:01 +0530

    update: add some big stars animation

---
+++ src/components/BlinkingStar.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import './BlinkingStar.css';
+
+const BlinkingStar = () => {
+    const [position, setPosition] = useState({ top: '50%', left: '50%' });
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const top = Math.random() * window.innerHeight + 'px';
+            const left = Math.random() * window.innerWidth + 'px';
+            setPosition({ top, left });
+        }, 2000); // Change position every 2 seconds
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return <div className="star" style={position}></div>;
+};
+
+export default BlinkingStar;
