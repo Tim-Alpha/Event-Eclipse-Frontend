a750c24b796e8eff5d59dae76be0360c3b1418bd
Sachin Kinha <codewithaisha@gmail.com>
2024-07-06 11:55:45 +0530

    refactor: header & home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,89 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import React from "react";
 
 const Header = () => {
-    const [loginBtn, setLoginBtn] = useState('Sign-Up');
-    const navigate = useNavigate();
-    const [isMobile, setIsMobile] = useState(false);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 550);
-        };
-
-        handleResize(); // Call it initially to set isMobile
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    const onSignUp = () => {
-        navigate('/auth');
-        setLoginBtn('Login');
-    };
 
     return (
         <>
-            {isMobile ? (
-                <nav className="navbar bg-body-tertiary fixed-top">
-                    <div className="container-fluid">
-                        <Link className="navbar-brand" to="#">Event Eclipse</Link> {/* Use Link instead of a */}
-                        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                    </div>
-                </nav>
-            ) : (
-                <nav className="navbar bg-body-tertiary">
-                    <div className="container-fluid">
-                        <Link className="navbar-brand" to="#">Event Eclipse</Link> {/* Use Link instead of a */}
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                        <button className="btn btn-outline-success ms-2" onClick={onSignUp}>{loginBtn}</button>
-                    </div>
-                </nav>
-            )}
-
-            <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div className="offcanvas-body">
-                    <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="#">Home</Link> {/* Use Link instead of a */}
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="#">Link</Link> {/* Use Link instead of a */}
-                        </li>
-                        <li className="nav-item dropdown">
-                            <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> {/* Use Link instead of a */}
-                                Dropdown
-                            </Link>
-                            <ul className="dropdown-menu">
-                                <li><Link className="dropdown-item" to="#">Action</Link></li> {/* Use Link instead of a */}
-                                <li><Link className="dropdown-item" to="#">Another action</Link></li> {/* Use Link instead of a */}
-                                <li>
-                                    <hr className="dropdown-divider" />
-                                </li>
-                                <li><Link className="dropdown-item" to="#">Something else here</Link></li> {/* Use Link instead of a */}
-                            </ul>
-                        </li>
-                    </ul>
-                    <form className="d-flex mt-3" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                    <div className="w-90 mx-auto mt-3"> {/* Style to make the button 90% width and center it */}
-                        <button className="btn btn-outline-success w-100" onClick={onSignUp}>{loginBtn}</button>
-                    </div>
-                </div>
-            </div>
+            <nav>
+                <div className='logo' ><span className='logo-1' >Event</span><span className='logo-2' >Ecliplse</span></div>
+            </nav>
         </>
     );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,58 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import Container from './Container';
-import Footer from './Footer';
-import TopContainer from './TopContainer';
-import img1 from './images/img1.jpg';
-import img2 from './images/img2.jpg';
-import img3 from './images/img3.jpeg';
-import img4 from './images/img4.jpeg';
-import img5 from './images/img5.jpeg';
-import img6 from './images/img6.jpeg';
-import img7 from './images/img7.webp';
-import img8 from './images/img8.webp';
+import React from 'react';
 import Header from './Header';
-import SignupPopup from './SignupPopup';
-import { isLoggedIn } from '../helper/auth';
+import StarField from '../StarField';
 
 const Home = () => {
-  const [showPopup, setShowPopup] = useState(false);
-
-  useEffect(() => {
-    if (!isLoggedIn()) {
-      const timer = setTimeout(() => {
-        setShowPopup(true);
-      }, 10000);
-
-      return () => clearTimeout(timer);
-    }
-  }, []);
-
-  const handleClosePopup = () => {
-    setShowPopup(false);
-  };
 
   return (
-    <>
-      <Header />
-      <TopContainer />
-      <div className="container mt-4">
-        <div className="row">
-          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img1} />
-          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img2} />
-          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img3} />
-          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img4} />
-          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img5} />
-          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img6} />
-          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img7} />
-          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img8} />
-          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img1} />
-          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img2} />
-          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img3} />
-          <Container newClass="col-12 col-md-6 col-lg-3 mb-4" width="100" height="40" background="" corner="8" link={img4} />
-        </div>
+    <>    
+    <Header />
+    <div className='home-page'>
+      <StarField />
+      <div className='hero-section' >
+        <h1 className='hero-heading' >Event Ecliplse</h1>
+        <p className='hero-text' >Welcome to EventEcliplse, The Revolutionary Digital Heaven for finding and booking event venues</p>
       </div>
-      <Footer />
-      {showPopup && <SignupPopup onClose={handleClosePopup} />}
+    </div>
     </>
   );
 };
