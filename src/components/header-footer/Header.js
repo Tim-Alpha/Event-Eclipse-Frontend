2d4d3da1151a508d0ee13557db46c066fefe2827
Sachin Kinha <codewithaisha@gmail.com>
2024-07-07 00:27:35 +0530

    update: add login btn in header

--- src/components/header-footer/Header.js
+++ src/components/header-footer/Header.js
@@ -1,14 +1,23 @@
-import React from "react";
-
-const Header = () => {
-
-    return (
-        <>
-            <nav>
-                <div className='logo' ><span className='logo-1' >Event</span><span className='logo-2' >Ecliplse</span></div>
-            </nav>
-        </>
-    );
-};
-
+import React from "react"; 
+import { useNavigate } from "react-router-dom"; 
+import "../css/Header.css" 
+const Header = () => { 
+    const navigate = useNavigate(); 
+ 
+    return ( 
+        <> 
+            <nav> 
+            <div className='logo' onClick={() => navigate('/')}> 
+                    <span className='logo-1'>Event</span> 
+                    <span className='logo-2'>Eclipse</span> 
+                </div> 
+                <div className='auth-buttons'> 
+                    <button className='login-button' onClick={() => navigate('/login')}>Login</button> 
+                    <button className='registration-button' onClick={() => navigate('/register')}>Register</button> 
+                </div> 
+            </nav> 
+        </> 
+    ); 
+}; 
+ 
 export default Header;
