277b3b0976034345e5f6f7a997a61908dc3a712e
Sachin Kinha <codewithaisha@gmail.com>
2024-07-07 15:08:41 +0530

    update: add section in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// import Fireworks from './Fireworks';
 import Header from './header-footer/Header';
 import StarField from './animation/stars/StarField';
 import BlinkingStar from './animation/stars/BlinkingStar';
 import FireworksBackground from './animation/fireworks/FireworksBackground';
+import Footer from './header-footer/Footer';
 
 const Home = () => {
 
@@ -13,13 +13,33 @@ const Home = () => {
     <div className='home-page'>
       <StarField />
       <BlinkingStar />
-      {/* <Fireworks /> */}
       <FireworksBackground />
       <div className='hero-section' >
-        <h1 className='hero-heading' >Event Ecliplse</h1>
-        <p className='hero-text' >Welcome to EventEcliplse, The Revolutionary Digital Heaven for finding and booking event venues</p>
+        <div className='hero-main-heading'>
+          <h1 className='hero-heading' >Event Eclipse</h1>
+          <p className='hero-text' >Welcome to EventEclipse, The Revolutionary Digital Heaven for finding and booking event venues</p>
+        </div>
       </div>
-    </div>
+      <div className='about-section' >
+        <div className='hero-main-heading'>
+          <h1 className='hero-heading' >Why We?</h1>
+          <p className='hero-text' >Welcome to EventEclipse, The Revolutionary Digital Heaven for finding and booking event venues</p>
+        </div>
+      </div>
+      <div className='service-section' >
+        <div className='hero-main-heading'>
+          <h1 className='hero-heading' >Venue Near You</h1>
+          <p className='hero-text' >Welcome to EventEclipse, The Revolutionary Digital Heaven for finding and booking event venues</p>
+        </div>
+      </div>
+      <div className='customer-section' >
+        <div className='hero-main-heading'>
+          <h1 className='hero-heading' >Happy Customers</h1>
+          <p className='hero-text' >Welcome to EventEclipse, The Revolutionary Digital Heaven for finding and booking event venues</p>
+        </div>
+      </div>
+    </div> 
+    <Footer />
     </>
   );
 };
