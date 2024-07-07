29bd3e02ad844e5d40ef3ae779ef0ed0075ad84c
Sachin Kinha <codewithaisha@gmail.com>
2024-07-07 15:09:38 +0530

    update: add footer

--- src/components/header-footer/Footer.js
+++ src/components/header-footer/Footer.js
@@ -1,37 +1,14 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { FaHome, FaComment, FaHistory } from "react-icons/fa";
+import React from 'react';
+import '../css/Footer.css';
 
-const BottomBar = () => {
-    const navigate = useNavigate();
-
-    const handleHomeClick = () => {
-        navigate("/");
-    };
-
-    const handleChatClick = () => {
-        navigate("/chat");
-    };
-
-    const handleHistoryClick = () => {
-        navigate("/history");
-    };
-
+const Footer = () => {
     return (
-        <footer className="bottom-bar bg-dark text-white text-center py-3">
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <ul className="icon-list">
-                            <li onClick={handleHomeClick}><FaHome /></li>
-                            <li onClick={handleChatClick}><FaComment /></li>
-                            <li onClick={handleHistoryClick}><FaHistory /></li>
-                        </ul>
-                    </div>
-                </div>
+        <footer className='footer'>
+            <div className='footer-section'>
+                <p className='footer-text'><span className='copyright'>&copy;</span> copyright 2024 Event Eclipse</p>
             </div>
         </footer>
     );
 };
 
-export default BottomBar;
+export default Footer;
